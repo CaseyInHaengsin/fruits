dc3b4ba0535782b5d78be9166b4399c920619fc6
caseyinhaengsin <rowley.james74@gmail.com>
2020-10-07 19:59:30 -0600

    accepting posts.

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -8,7 +8,25 @@ class Body extends React.Component {
       }
 
       handleFormSubmit(name, description){
+        
+        let body = JSON.stringify({fruit: {name: name, description:   description} })
+        fetch('api/v1/fruits', {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: body,
+        }).then((res) => {return res.json()})
+          .then((fruit) => {
+            this.addNewFruit(fruit);
+          })
         console.log(name, description)
+      }
+
+      addNewFruit(fruit){
+        this.setState({
+          fruits: this.state.fruits.concat(fruit)
+        })
       }
 
     componentDidMount(){
