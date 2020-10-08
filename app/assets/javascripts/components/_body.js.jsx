572307e4a85e0690a0f9c76ae81a2da9a40e205b
caseyinhaengsin <rowley.james74@gmail.com>
2020-10-07 19:53:48 -0600

    logging submission to console.

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -4,7 +4,13 @@ class Body extends React.Component {
         this.state = {
           fruits: []
         };
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
       }
+
+      handleFormSubmit(name, description){
+        console.log(name, description)
+      }
+
     componentDidMount(){
         fetch('/api/v1/fruits')
           .then((response) => {return response.json()})
@@ -13,7 +19,7 @@ class Body extends React.Component {
     render(){
         return(
           <div>
-            <NewFruit />
+            <NewFruit handleFormSubmit={this.handleFormSubmit}/>
             <AllFruits fruits={this.state.fruits} />
           </div>
         )
--- app/assets/javascripts/components/_new_item.js.jsx
+++ app/assets/javascripts/components/_new_item.js.jsx
@@ -2,7 +2,15 @@ const NewFruit = (props) => {
     let formFields = {}
    
     return(
-      <form className="ui form">
+      <form className="ui form" onSubmit={(e) => {
+        e.preventDefault();
+        props.handleFormSubmit(
+          formFields.name.value,
+          formFields.description.value
+        )
+
+        e.target.reset();
+      }}>
        <input ref={input => formFields.name = input} placeholder='Enter the name of the item'/>
        <input ref={input => formFields.description = input} placeholder='Enter a description' />
        <button className="ui button">Submit</button>
